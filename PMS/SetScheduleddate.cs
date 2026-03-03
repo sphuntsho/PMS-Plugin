255f718c3e492b78dcdf231ff061df8278781def
SP <69109172+sphuntsho@users.noreply.github.com>
2026-03-03 16:02:15 +1100

    Clean up comments in SetScheduledDate plugin
    Removed unnecessary comments to improve code readability.

--- PMS/SetScheduleddate.cs
+++ PMS/SetScheduleddate.cs
@@ -7,30 +7,29 @@ namespace PMS
     {
         public void Execute(IServiceProvider serviceProvider)
         {
-            // Obtain the tracing service
+            
             ITracingService tracingService =
             (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
-            // Obtain the execution context from the service provider.
+           
             IPluginExecutionContext context = (IPluginExecutionContext)
                 serviceProvider.GetService(typeof(IPluginExecutionContext));
 
-            // The InputParameters collection contains all the data passed in the message request.
+           
             if (context.InputParameters.Contains("Target") &&
                 context.InputParameters["Target"] is Entity)
             {
-                // Obtain the target entity from the input parameters.
+                
                 Entity entity = (Entity)context.InputParameters["Target"];
 
-                // Obtain the IOrganizationService instance which you will need for
-                // web service calls.
+              
                 IOrganizationServiceFactory serviceFactory =
                     (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                 IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
                 try
                 {
-                    // Plug-in business logic goes here.
+                   
                     if (context.MessageName != "pms_SetScheduleddate") return;
 
                     if (entity is null || entity.LogicalName != "pms_inspection") return;
